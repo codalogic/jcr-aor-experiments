77e1a11ebbf28652bacfc2a5e3d0038eaa9e67c8
Pete Cordell <git@codalogic.com>
2018-09-13 20:34:03 +0100

    Add Validator class to project

--- detail/validator.rb
+++ detail/validator.rb
@@ -22,41 +22,12 @@
 #
 # For info see https://github.com/codalogic/jcr-aor-experiments
 
-require_relative 'detail/pattern_tokeniser'
-require_relative 'detail/pattern'
-require_relative 'detail/instance'
-require_relative 'detail/validator'
+require_relative 'pattern'
+require_relative 'instance'
 
-$pattern = nil
-
-def main
-    Dir.glob( 'jcr_aor*.txt' ) { |fname| process_file fname }
-end
-
-def process_file fname
-    line_num = 1
-    File.foreach( fname ) { |line| process_line line_num, line; line_num += 1 }
-end
-
-def process_line line
-    case line[0]
-        when '?'
-            parse_pattern line[1..-1]
-        when '+'
-            test_valid_instance line[1..-1]
-        when '-'
-            test_invalid_instance line[1..-1]
+class Validator
+    def initialize pattern
+    end
+    def valid? instance
     end
 end
-
-def test_valid_instance line
-    instance = Instance.new line
-    puts instance[1]
-    # TODO
-end
-
-def test_invalid_instance line
-    # TODO
-end
-
-main if __FILE__ == $PROGRAM_NAME
--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -22,41 +22,14 @@
 #
 # For info see https://github.com/codalogic/jcr-aor-experiments
 
-require_relative 'detail/pattern_tokeniser'
-require_relative 'detail/pattern'
-require_relative 'detail/instance'
-require_relative 'detail/validator'
+require 'rspec'
+require_relative '../jcr_aor'
 
-$pattern = nil
+describe 'Validator class' do
+    context 'basic behaviour' do
 
-def main
-    Dir.glob( 'jcr_aor*.txt' ) { |fname| process_file fname }
-end
-
-def process_file fname
-    line_num = 1
-    File.foreach( fname ) { |line| process_line line_num, line; line_num += 1 }
-end
-
-def process_line line
-    case line[0]
-        when '?'
-            parse_pattern line[1..-1]
-        when '+'
-            test_valid_instance line[1..-1]
-        when '-'
-            test_invalid_instance line[1..-1]
+        it 'should be creatable' do
+            v = Validator.new Pattern.new
+        end
     end
 end
-
-def test_valid_instance line
-    instance = Instance.new line
-    puts instance[1]
-    # TODO
-end
-
-def test_invalid_instance line
-    # TODO
-end
-
-main if __FILE__ == $PROGRAM_NAME
